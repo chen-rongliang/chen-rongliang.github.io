adcbbb6a39a780ef4bace85faecc8b4625423566
chen-rongliang <chen-rongliang@qq.com>
2019-11-21 10:25:41 +0800

    build

--- src/axios/path.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-// 全局接口
-
-module.exports = {
-    suggest: 'https://m.baidu.com/su',
-    weather: 'https://widget-page.heweather.net/h5/index.html?bg=1&md=04&lc=accu&key=1b9a99e5b9c94d608060f542e23b8426'
-}
